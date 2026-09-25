4fe85d70ec92ab88aa5f66ea313813c7384db749
frechdax <basti.live@gmail.com>
2026-09-25 22:19:34 +0200

    Include Chromium binaries in live-data function

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
   serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+  outputFileTracingIncludes: {
+    "/api/live-daten": ["./node_modules/@sparticuz/chromium/bin/**"],
+  },
 };
 
 export default nextConfig;
